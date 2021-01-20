15d9a50835c7e02b6d83f1d84706b23d68d123ad
Marcos Vinicius <marcospuglliesyprogramer@gmail.com>
2021-01-19 21:56:48 -0300

    update files

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ const values = {
 	value_percents_css: 80,
 	value_percents_html: 70,
 	value_percents_js: 50,
+	value_percents_en: 30,
 };
 
 const elements = {
 	value_percents_css: [document.querySelector(".value_percents_css"),document.querySelector(".percents_css")],
 	value_percents_html: [document.querySelector(".value_percents_html"),document.querySelector(".percents_html")],
 	value_percents_js: [document.querySelector(".value_percents_js"),document.querySelector(".percents_js")],
+	value_percents_en: [document.querySelector(".value_percents_en"),document.querySelector(".percents_en")],
 };
 
 for (let el in elements) {
