8ac650f74a9b7e9f2a803fb259704549bcc26503
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-01-27 10:18:08 -0300

    update in some colors and fields

--- js/script.js
+++ js/script.js
@@ -1,81 +1,26 @@
-const functions = {
-  style: {
-    //default: `
-    //  transform: scale(1.0);
-    //  background-color: rgba(217, 227, 247, 0.5);
-    //  color: white;
-    //`,
-    release: `
-      transform: scale(0.8);
-      background-color: #4b4b4b;
-      color: lightgray;
-    `,
-  },
+const form = document.querySelector('.form_box');
 
-  insertDataInCards: () => {
-    const values = {
-      value_percents_css: 80,
-      value_percents_html: 70,
-      value_percents_js: 50,
-      value_percents_en: 30,
-    };
+form.addEventListener('submit', (e) => {
+  let input_name = document.querySelector('.name')
+  let input_email = document.querySelector('.email')
 
-    const elements = {
-      value_percents_css: [
-        document.querySelector(".value_percents_css"),
-        document.querySelector(".percents_css"),
-      ],
-      value_percents_html: [
-        document.querySelector(".value_percents_html"),
-        document.querySelector(".percents_html"),
-      ],
-      value_percents_js: [
-        document.querySelector(".value_percents_js"),
-        document.querySelector(".percents_js"),
-      ],
-      value_percents_en: [
-        document.querySelector(".value_percents_en"),
-        document.querySelector(".percents_en"),
-      ],
-    };
-
-    for (let el in elements) {
-      let element = elements[el][0];
-      let l_visual = elements[el][1].querySelector("span");
-      let len = values[el];
-
-      element.innerText = values[el];
-      l_visual.style.display = "block";
-      l_visual.style.minWidth = `${len}%`;
-      l_visual.style.maxWidth = `${len}%`;
-    }
-  },
-
-  apply_style: (element, style) => {
-    element.style += style;
-  },
-
-  hoverCards: () => {
-    const all_cards = document.querySelectorAll(".card");
-
-    all_cards.forEach((card) => {
-      card.addEventListener("mouseenter", () => {
-        card.style.backgroundColor = "#2980b9";
-        all_cards.forEach((card_2) => {
-          if (card != card_2) {
-            functions.apply_style(card_2, functions.style.release);
-          }
-        });
-      });
-      card.addEventListener("mouseout", () => {
-        card.style.backgroundColor = "rgba(217, 227, 247, 0.5)";
-        all_cards.forEach((card_2) => {
-						card_2.style = ""
-        });
-      });
-    });
-  },
-};
-
-functions.insertDataInCards();
-functions.hoverCards();
+  if (input_email.value.length === 0 || input_name.value.length === 0) {
+    e.preventDefault();
+    alert('name or email is invalid!');
+    input_name.value = "";
+    input_email.value = "";
+  }
+  if (input_name.value.length < 3) {
+    e.preventDefault();
+    alert('name very short, min lenght is 3');
+    input_name.value = "";
+  }
+  if (input_email.value.length > 254) {
+    e.preventDefault()
+    alert('email is invalid!');
+  }
+  if (input_email.value.length < 3) {
+    e.preventDefault()
+    alert('email is invalid');
+  }
+})
