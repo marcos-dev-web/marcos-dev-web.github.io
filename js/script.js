8d9a74f456f9dbf4db3936770d660b024bfb45af
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-01-30 11:01:16 -0300

    solved bug

--- js/script.js
+++ js/script.js
@@ -8,28 +8,33 @@ const footer = q('footer');
 const form_section = q('.form');
 
 form.addEventListener('submit', (e) => {
-  let input_name = document.querySelector('.name')
-  let input_email = document.querySelector('.email')
+  let input_name = q('#name')
+  let input_email = q('#email')
 
   if (input_email.value.length === 0 || input_name.value.length === 0) {
     e.preventDefault();
     alert('name or email is invalid!');
     input_name.value = "";
     input_email.value = "";
+    return;
   }
   if (input_name.value.length < 3) {
     e.preventDefault();
     alert('name very short, min lenght is 3');
     input_name.value = "";
+    return;
   }
   if (input_email.value.length > 254) {
     e.preventDefault()
     alert('email is invalid!');
+    return;
   }
   if (input_email.value.length < 3) {
     e.preventDefault()
     alert('email is invalid');
+    return;
   }
+  e.preventDefault()
 })
 
 setTimeout(() => {
