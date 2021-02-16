104333d5308cb31ef2f9a931ddc0802f1966b532
marcos-dev-web <marcospuglliesyprogramer@gmail.com>
2021-02-16 19:20:26 -0300

    added transition in hide header

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,8 @@ const header_top = q('.profile__img');
 
 $(window).bind('mousewheel', (e) => {
   if (e.originalEvent.wheelDelta >= 0) {
-    header_top.style.top = '0';
+    header_top.style.transform = 'translateY(0)';
   } else {
-    header_top.style.top = '-100%';
+    header_top.style.transform = 'translateY(-100%)';
   }
 })
